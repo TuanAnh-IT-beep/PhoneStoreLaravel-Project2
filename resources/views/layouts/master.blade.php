ad489f45bc877509185ca2692f92da7c07145d94
marseio <pdnghiapdnghia@gmail.com>
2026-03-19 21:36:52 +0700

    modified:   public/css/admin.css 	modified:   resources/views/layouts/master.blade.php 	modified:   resources/views/layouts/sidebar.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,44 @@
+<div class="flex flex-col min-h-screen">
 @include("layouts.header")
-<div class="container mx-auto flex" style="gap: 20px">
+<div class="container mx-auto flex flex-1" style="gap: 20px">
     <!-- Sidebar -->
     @include("layouts.sidebar")
     <!-- Main Content -->
-    <div class="main-content">
-        <h1 style="color: black; font-size: 24px; font-family: Rubik; font-weight: 700; word-wrap: break-word">
-            Dashboard</h1>
+    <div class="main-content flex-1 p-6">
+        <h1>Title</h1>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
     </div>
 </div>
 
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,26 +1,26 @@
-    <div class="sidebar">
-        <a class="item active">
-            <img class="icon" src="images/main/sidebar/dashboard.png" />
-            <p class="text">Dashboard</p>
-        </a>
-        <a class="item">
-            <img class="icon" src="images/main/sidebar/category.png" />
-            <p class="text">Categories</p>
-        </a>
-        <a class="item">
-            <img class="icon" src="images/main/sidebar/brand.png" />
-            <p class="text">Brands</p>
-        </a>
-        <a class="item">
-            <img class="icon" src="images/main/sidebar/product.png" />
-            <p class="text">Products</p>
-        </a>
-        <a class="item">
-            <img class="icon" src="images/main/sidebar/order.png" />
-            <p class="text">Orders</p>
-        </a>
-        <a class="item">
-            <img class="icon" src="images/main/sidebar/admin.png" />
-            <p class="text">Admins</p>
-        </a>
-    </div>
+<div class="sidebar">
+    <a class="item active" href="">
+        <img class="icon" src="images/main/sidebar/dashboard.png" />
+        <p class="text">Dashboard</p>
+    </a>
+    <a class="item" href="">
+        <img class="icon" src="images/main/sidebar/category.png" />
+        <p class="text">Categories</p>
+    </a>
+    <a class="item" href="">
+        <img class="icon" src="images/main/sidebar/brand.png" />
+        <p class="text">Brands</p>
+    </a>
+    <a class="item" href="">
+        <img class="icon" src="images/main/sidebar/product.png" />
+        <p class="text">Products</p>
+    </a>
+    <a class="item" href="">
+        <img class="icon" src="images/main/sidebar/order.png" />
+        <p class="text">Orders</p>
+    </a>
+    <a class="item" href="">
+        <img class="icon" src="images/main/sidebar/admin.png" />
+        <p class="text">Admins</p>
+    </a>
+</div>
