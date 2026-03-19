15b938c62f6335d4abf3bfb9df8dadfd57b982e0
marseio <pdnghiapdnghia@gmail.com>
2026-03-19 20:56:25 +0700

    modified:   resources/views/layouts/sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,26 +1,26 @@
     <div class="sidebar">
-        <div class="item active">
+        <a class="item active">
             <img class="icon" src="images/main/sidebar/dashboard.png" />
             <p class="text">Dashboard</p>
-        </div>
-        <div class="item">
+        </a>
+        <a class="item">
             <img class="icon" src="images/main/sidebar/category.png" />
             <p class="text">Categories</p>
-        </div>
-        <div class="item">
+        </a>
+        <a class="item">
             <img class="icon" src="images/main/sidebar/brand.png" />
             <p class="text">Brands</p>
-        </div>
-        <div class="item">
+        </a>
+        <a class="item">
             <img class="icon" src="images/main/sidebar/product.png" />
             <p class="text">Products</p>
-        </div>
-        <div class="item">
+        </a>
+        <a class="item">
             <img class="icon" src="images/main/sidebar/order.png" />
             <p class="text">Orders</p>
-        </div>
-        <div class="item">
+        </a>
+        <a class="item">
             <img class="icon" src="images/main/sidebar/admin.png" />
             <p class="text">Admins</p>
-        </div>
+        </a>
     </div>
