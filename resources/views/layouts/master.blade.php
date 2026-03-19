15ecec15e0df7823e4db4c581ea4b04a59aef2df
marseio <pdnghiapdnghia@gmail.com>
2026-03-19 20:55:57 +0700

    modified:   resources/views/layouts/master.blade.php 	modified:   resources/views/layouts/sidebar.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,32 +1,7 @@
 @include("layouts.header")
 <div class="container mx-auto flex" style="gap: 20px">
     <!-- Sidebar -->
-    <div class="sidebar">
-        <div class="item active">
-            <img class="icon" src="images/main/sidebar/dashboard.png" />
-            <p class="text">Dashboard</p>
-        </div>
-        <div class="item">
-            <img class="icon" src="images/main/sidebar/category.png" />
-            <p class="text">Categories</p>
-        </div>
-        <div class="item">
-            <img class="icon" src="images/main/sidebar/brand.png" />
-            <p class="text">Brands</p>
-        </div>
-        <div class="item">
-            <img class="icon" src="images/main/sidebar/product.png" />
-            <p class="text">Products</p>
-        </div>
-        <div class="item">
-            <img class="icon" src="images/main/sidebar/order.png" />
-            <p class="text">Orders</p>
-        </div>
-        <div class="item">
-            <img class="icon" src="images/main/sidebar/admin.png" />
-            <p class="text">Admins</p>
-        </div>
-    </div>
+    @include("layouts.sidebar")
     <!-- Main Content -->
     <div class="main-content">
         <h1 style="color: black; font-size: 24px; font-family: Rubik; font-weight: 700; word-wrap: break-word">
