6247f6256149d78292d9ba41542efdeeabcb5b3d
tuananh <cactustg252006@gmail.com>
2026-03-20 16:58:08 +0700

    modified:   resources/views/layouts/sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,28 +1,4 @@
 <div class="sidebar">
-<<<<<<< HEAD
-    <a class="item active" href="./">
-        <img class="icon" src="images/main/sidebar/dashboard.png" />
-        <p class="text">Dashboard</p>
-    </a>
-    <a class="item" href="{{ route(name:'categories.index') }}">
-        <img class="icon" src="images/main/sidebar/category.png" />
-        <p class="text">Categories</p>
-    </a>
-    <a class="item" href="{{ route(name:'manufacturers.index') }}">
-        <img class="icon" src="images/main/sidebar/brand.png" />
-        <p class="text">Manufacturers</p>
-    </a>
-    <a class="item" href="{{ route(name:'subproducts.index') }}">
-        <img class="icon" src="images/main/sidebar/product.png" />
-        <p class="text">Products</p>
-    </a>
-    <a class="item" href="{{ route(name:'orders.index') }}">
-        <img class="icon" src="images/main/sidebar/order.png" />
-        <p class="text">Orders</p>
-    </a>
-    <a class="item" href="{{ route(name:'users.index') }}">
-        <img class="icon" src="images/main/sidebar/admin.png" />
-=======
     <a class="item active" href="">
         <img class="icon" src="/images/main/sidebar/dashboard.png" />
         <p class="text">Dashboard</p>
@@ -45,7 +21,6 @@
     </a>
     <a class="item" href="">
         <img class="icon" src="/images/main/sidebar/admin.png" />
->>>>>>> 6a921e13ffd76ead4ef1f06cbb5fa4ac35e364fd
         <p class="text">Admins</p>
     </a>
 </div>
