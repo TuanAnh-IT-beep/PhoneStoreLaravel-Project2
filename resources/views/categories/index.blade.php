6a921e13ffd76ead4ef1f06cbb5fa4ac35e364fd
marseio <pdnghiapdnghia@gmail.com>
2026-03-20 16:55:30 +0700

    modified:   public/css/admin.css 	modified:   resources/views/categories/create.blade.php 	modified:   resources/views/categories/index.blade.php 	modified:   resources/views/customers/index.blade.php 	modified:   resources/views/layouts/head.blade.php 	modified:   resources/views/layouts/sidebar.blade.php

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,41 +1,44 @@
 @extends("layouts.master")
 
 @section("main-content")
-    <div class="w-full flex mb-4 justify-between">
-        <h1 class="">Categories</h1>
-        <a class="btn" href="{{ route(name: 'categories.create') }}"><i class="fa-solid fa-plus"></i>ADD NEW ITEM</a>
+    <div class="w-full mb-4 flex items-center justify-between">
+        <h1>Categories</h1>
+        <a class="btn icon-only" href="{{ route(name: 'categories.create') }}"><i class="fa-solid fa-plus"></i> ADD NEW ITEM</a>
     </div>
-    @if (count($categories)) > 0
-        <table border="1px" cellpadding="0" cellspacing="0" width="100%">
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th></th>
-            </tr>
-            @foreach ($categories as $category)
+    <div class="main-container">
+        <table class="table-auto w-full text-left rtl:text-right text-body">
+            <thead class="border-default">
                 <tr>
-                    <td>
-                        {{ $category->id }}
-                    </td>
-                    <td>
-                        {{ $category->name }}
-                    </td>
-                    <td>
-                        {{ $category->description }}
-                    </td>
-                    <td>
-                        <a href="{{ route('categories.edit', $category->id) }}">Edit</a>
-                        <form method="post" action="{{ route('categories.delete', $category->id) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button>Delete</button>
-                        </form>
-                    </td>
+                    <th scope="col" class="px-6 py-3 font-medium">ID</th>
+                    <th scope="col" class="px-6 py-3 font-medium">Name</th>
+                    <th scope="col" class="px-6 py-3 font-medium long">Description</th>
+                    <th scope="col" class="px-6 py-3 font-medium">Actions</th>
                 </tr>
-            @endforeach
+            </thead>
+            <tbody>
+                @foreach ($categories as $category)
+                    <tr scope="row" class="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                        <th class="px-6 py-4">
+                            {{ $category->id }}
+                        </th>
+                        <td class="px-6 py-4" style="color: black"> {{ $category->name }} </td>
+                        <td class="px-6 py-4">
+                            {{ $category->description }}
+                        </td>
+                        <td class="px-6 py-4">
+                            <form method="post" action="{{ route('categories.destroy', $category->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                <a class="btn edit icon-only" href="{{ route('categories.edit', $category->id) }}"><i
+                                        class="fa-solid fa-pencil"></i></a>
+                                <button class="btn delete icon-only"><i class="fa-solid fa-trash"></i></button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+
         </table>
-    @else
-        <p>No category found.</p>
-    @endif
+    </div>
+
 @endsection
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,26 +1,26 @@
 <div class="sidebar">
     <a class="item active" href="">
-        <img class="icon" src="images/main/sidebar/dashboard.png" />
+        <img class="icon" src="/images/main/sidebar/dashboard.png" />
         <p class="text">Dashboard</p>
     </a>
     <a class="item" href="">
-        <img class="icon" src="images/main/sidebar/category.png" />
+        <img class="icon" src="/images/main/sidebar/category.png" />
         <p class="text">Categories</p>
     </a>
     <a class="item" href="">
-        <img class="icon" src="images/main/sidebar/brand.png" />
+        <img class="icon" src="/images/main/sidebar/brand.png" />
         <p class="text">Brands</p>
     </a>
     <a class="item" href="">
-        <img class="icon" src="images/main/sidebar/product.png" />
+        <img class="icon" src="/images/main/sidebar/product.png" />
         <p class="text">Products</p>
     </a>
     <a class="item" href="">
-        <img class="icon" src="images/main/sidebar/order.png" />
+        <img class="icon" src="/images/main/sidebar/order.png" />
         <p class="text">Orders</p>
     </a>
     <a class="item" href="">
-        <img class="icon" src="images/main/sidebar/admin.png" />
+        <img class="icon" src="/images/main/sidebar/admin.png" />
         <p class="text">Admins</p>
     </a>
 </div>
