c2b2202fbf7ab818fac77036516261e0de09b23c
tuananh <cactustg252006@gmail.com>
2026-03-27 09:01:39 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is behind 'origin/main' by 3 commits, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   public/css/login.css
    	renamed:    resources/img/v1_30.png -> public/images/login/banner.png
    	deleted:    resources/img/v1_25.png
    	deleted:    resources/img/v1_44.png
    	modified:   resources/views/Login.blade.php

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -7,10 +7,22 @@
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
     <title>Login</title>
 </head>
-
 <body>
-    <div class="login_box">
-        
+    <div class="login_box grid grid-cols-2 gap-4">
+            <div>
+                <img src="/images/login/banner.png" alt="" style="height: 100%;">
+            </div>
+            <div style="padding-top:40px;padding-left:30px;padding-right:40px">
+                <h1 style="font-size: 27px;color:black;font-weight:bold">Login as Administrator</h1>
+                <div>
+                    <h3 style="color:rgb(125, 124, 124)">Username or E-mail</h3>
+                    <input type="text" placeholder="Enter your username" class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" style="background-color:white;color:black;border-color:rgb(169, 169, 169)">
+                </div>
+                <div>
+                    <h3 style="color:rgb(125, 124, 124)">Password</h3>
+                    <input type="text" placeholder="Enter password here.." class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" style="background-color:white;color:black;border-color:rgb(169, 169, 169)">
+                </div>
+            </div>
     </div>
 </body>
 </html>
