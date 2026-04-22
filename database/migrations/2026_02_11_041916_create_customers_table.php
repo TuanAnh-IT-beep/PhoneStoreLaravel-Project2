03e451f8328d2369f24c0f97d9608fa413c5e7fb
TuanAnh-IT-beep <cactustg252006@gmail.com>
2026-04-22 08:10:29 +0700

    modified:   .env.example 	modified:   database/migrations/2026_02_11_041916_create_customers_table.php 	modified:   package-lock.json 	modified:   resources/views/admins/layouts/head.blade.php 	modified:   resources/views/clients/index.blade.php

--- database/migrations/2026_02_11_041916_create_customers_table.php
+++ database/migrations/2026_02_11_041916_create_customers_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string("display_name");
             $table->string("email")->unique();
             $table->string("phone");
+            $table->string("gender")->nullable();
             $table->date("birthday")->nullable();
             $table->string("address")->nullable();
             $table->timestamps();
